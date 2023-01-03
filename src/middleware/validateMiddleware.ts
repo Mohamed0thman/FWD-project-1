dc14705b2a81ce370f3fca1e20209616fbc5e825
mohamed othman <@mohamed.0thman9988>
2023-01-03 23:52:34 +0200

    project

--- src/middleware/validateMiddleware.ts
+++ src/middleware/validateMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import fs from "fs";
 
 import { fullPath, thumpPath } from "../paths";
+import { isInt } from "../utiles/utiles";
 
 export const validateQuery = (
   request: Request,
@@ -14,6 +15,18 @@ export const validateQuery = (
     if (!request.query[item]) {
       throw {
         message: ` ${item} is required `,
+        status: 400,
+        error: new Error(),
+      };
+    }
+    console.log(item, /^\d+$/.test(request.query[item] as string));
+
+    if (
+      (item === "width" || item === "height") &&
+      !isInt(request.query[item] as string)
+    ) {
+      throw {
+        message: ` ${item} must be integer `,
         status: 400,
         error: new Error(),
       };
